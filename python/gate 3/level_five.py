b37143e088d55210ef870ad82b705e6cde81cc46
delightonyedikachipixel <orji.delight.onyedikachi@gmail.com>
2026-03-18 13:26:39 +0100

    gate 3

---
+++ python/gate 3/level_five.py
@@ -0,0 +1,29 @@
+def get_add_to_the_length_of_list_at_5(numbers):
+
+    numbers = [10,11,12]
+    numbers.apend(-1)
+    numbers.append(-1)
+    return numbers
+
+
+def get_remove_to_the_length_of_list_at_2(numbers):
+
+    numbers = [10,11,12,13,14]
+    numbers.pop[4]
+    numbers.pop[3]
+    numbers.pop[2]
+    return numbers
+
+
+def get_add_to_the_length_of_list_at_0(numbers):
+
+    numbers = []
+    numbers.append(-1)
+    numbers.append(-1)
+    numbers.append(-1)
+    numbers.append(-1)
+    return numbers
+
+def get_add_to_the_length_of_list_at_-2(numbers):
+    numbers[1,2,3,4,5]
+    return 0
---
+++ python/gate 3/test_level_five.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from level_five import *
+
+class TestClass(TestCase):
+
+        def test_get_add_to_the_length_of_list_at_5(self):
+            self.assertEqual(get_add_to_the_length_of_list_at_5([10,11,12,-1,-1])
+
+        def test_get_remove_to_the_length_of_list_at_2(self):
+            self.assertEqual(get_add_to_the_length_of_list_at_2([10,11])
+
+        def test_get_add_to_the_length_of_list_at_0(self):
+            self.assertEqual(get_add_to_the_length_of_list_at_0([-1,-1,-1,-1])
+
