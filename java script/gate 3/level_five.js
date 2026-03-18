4d09904b42984e4329630b5ea5b44b93cc3a9556
delightonyedikachipixel <orji.delight.onyedikachi@gmail.com>
2026-03-18 13:19:07 +0100

    gate 3

---
+++ java script/gate 3/level_five.js
@@ -0,0 +1,19 @@
+function add_length(numbers){
+    let numbers = {10,11,12};
+    console.log(10,11,12,-1,-1,-1)
+}
+
+
+
+function add_length(numbers){
+    let numbers = {10,11,12,13,14};
+    console.log(10,11)
+}
+
+
+function add_length(numbers){
+    let numbers = {};
+    console.log(-1,-1,-1,-1)
+}
+
+
